fdd827486c9f2fbd1b9ac5356ad43bdfa59234a8
zhujian <zhujianxyz@163.com>
2023-02-11 23:32:55 +0800

    4000 tokens

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -30,7 +30,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
       temperature: 0.6,
-      max_tokens: 1000,
+      max_tokens: 4000,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
     console.log('Q:', req.body.animal, '\nA:',completion.data.choices[0].text.trimStart());
